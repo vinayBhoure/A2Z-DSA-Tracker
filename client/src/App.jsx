70dd065d22dbc36a4b7fff35cb6a3a9ce905f724
Vinay Bhoure <bhoure21@gmail.com>
2024-04-09 12:38:02 +0530

    Creating routes for multiple pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Aside from "./components/Aside";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProblemPage from "./pages/ProblemPage";
+import DashBoard from "./pages/DashBoard";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import OTPverification from "./pages/OTPverification";
+import UserInfoCollege from "./pages/UserInfoCollege";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={`flex flex-col h-[100vh] overflow-x-hidden`}>
+      <Navbar />
+      <div className="flex grow">
+        <Aside />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/problems" element={<ProblemPage />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/verify" element={<OTPverification />} />
+          <Route path="/userinfo/collage" element={<UserInfoCollege />} />
+
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
